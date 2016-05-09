e60f764a28f2d997c9667ceb4b36e1ab13043467
kjlundsgaard <katie.lundsgaard@gmail.com>
2016-05-09 15:40:14 -0700

    working on login functionality, added a couple html templates

--- server.py
+++ server.py
@@ -9,6 +9,9 @@ from model import connect_to_db, db
 # import Classes from model db
 from model import User, Group, UserGroup, List, Restaurant, RestaurantList, Address, Review
 
+# if I want to keep db functions in a separate file, could use this?
+import seed
+
 app = Flask(__name__)
 
 # Required to use Flask sessions and the debug toolbar
@@ -18,6 +21,58 @@ app.secret_key = "supersecretkey"
 # This is horrible. Fix this so that, instead, it raises an error.
 app.jinja_env.undefined = StrictUndefined
 
+
+@app.route('/')
+def index():
+    """Homepage."""
+
+    return render_template('index.html', login=session.get('user'))
+
+
+@app.route('/login')
+def login():
+    """Prompts user for login info"""
+
+    return render_template('login_form.html', login=session.get('user'))
+
+@app.route('/login', methods=['POST'])
+def submit_login():
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+    fname = request.form.get("fname")
+    lname = request.form.get("lname")
+
+    user = User.query.filter_by(email=username).first()
+    # if user already exists, checks password and logs them in if correct. If not, prompts
+    # for password again
+    if user:
+        if user.password == password:
+            session['user'] = user.user_id
+            flash("You are now logged in")
+            return redirect('/')
+            # return redirect('/users/' + str(user.user_id))
+        else:
+            flash("Password incorrect")
+            return redirect('/login')
+    else:
+        #instantiates new user and passes user_id to session
+        user = User(email=username, password=password, fname=fname, lname=lname)
+        db.session.add(user)
+        db.session.commit()
+        session['user'] = user.user_id
+        flash("Your account has been created")
+        return redirect('/')
+
+@app.route('/logout')
+def logout():
+    """Logs user out"""
+
+    session['user'] = None
+
+    flash ("You are now logged out")
+
+    return redirect('/')
 
 
 
